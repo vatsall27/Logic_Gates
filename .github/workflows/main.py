8b67e8156be70148701a235da780e8c64013598a
vatsall27 <128627104+vatsall27@users.noreply.github.com>
2023-03-25 10:39:17 +0530

    main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,24 @@
+def OR(A, B):
+	return A | B	
+
+print("Output of 0 OR 0 is", OR(0, 0))
+print("Output of 0 OR 1 is", OR(0, 1))
+print("Output of 1 OR 0 is", OR(1, 0))
+print("Output of 1 OR 1 is", OR(1, 1))
+
+
+
+def AND(A, B):
+	return A & B	
+
+print("Output of 0 AND 0 is", AND(0, 0))
+print("Output of 0 AND 1 is", AND(0, 1))
+print("Output of 1 AND 0 is", AND(1, 0))
+print("Output of 1 AND 1 is", AND(1, 1))
+
+
+def NOT(A):
+	return ~A+2	
+
+print("Output of NOT 0 is", NOT(0))
+print("Output of NOT 1 is", NOT(1))
